76d66b40dec0b9b72777f8cb41811c2a003dd9c6
Alberto Galiana <alberto.galiana@fon.com>
2019-02-14 00:39:14 +0100

    working on movies header

---
+++ lib/movies/ui/widgets/movies_header_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/movies/models/movie_model.dart';
+
+class MoviesHeaderWidget extends SliverPersistentHeaderDelegate {
+  final MovieModel _movie;
+
+  MoviesHeaderWidget(this._movie);
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return GridTile(
+        footer: GridTileBar(
+            backgroundColor: Colors.black54,
+            title: Text(_movie.originalTitle,
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 28.0)),
+            subtitle: Text(_movie.overview,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(color: Colors.white, fontSize: 14.0)),
+            trailing: Text(
+              '${_movie.voteAverage}/10',
+              style: new TextStyle(color: Colors.amberAccent, fontSize: 20.0),
+            )),
+        child: Image.network(
+          'https://image.tmdb.org/t/p/w185${_movie.posterPath}',
+          fit: BoxFit.cover,
+        ));
+  }
+
+  @override
+  double get maxExtent => 500.0;
+
+  @override
+  double get minExtent => 0.0;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
--- lib/movies/ui/widgets/movies_widget.dart
+++ lib/movies/ui/widgets/movies_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/movies/models/paginated_movies_model.dart';
+import 'package:flutter_app/movies/ui/widgets/movies_header_widget.dart';
 import 'package:flutter_app/movies/ui/widgets/movies_item_widget.dart';
 
 class MoviesWidget extends StatelessWidget {
@@ -14,11 +15,10 @@ class MoviesWidget extends StatelessWidget {
       controller: _scrollController,
       slivers: <Widget>[
         SliverPersistentHeader(
-          delegate: MyCollapsingImage(
-              'https://image.tmdb.org/t/p/w185${_paginatedMovies.movies[0].posterPath}'),
+          delegate: MoviesHeaderWidget(_paginatedMovies.movies[0]),
         ),
         SliverPadding(
-            padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0, bottom: 70.0),
+            padding: const EdgeInsets.only(top: 4.0, left: 0.0, right: 0.0, bottom: 70.0),
             sliver: SliverGrid(
               gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                 maxCrossAxisExtent: 180.0,
@@ -38,44 +38,5 @@ class MoviesWidget extends StatelessWidget {
             )),
       ],
     );
-
-    /*return GridView.builder(
-        itemCount: _paginatedMovies.movies.length,
-        padding: const EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0, bottom: 70.0),
-        //gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: (MediaQuery.of(context).size.width / 225).ceil(),
-          childAspectRatio: 0.75,
-        ),
-        controller: _scrollController,
-        physics: const AlwaysScrollableScrollPhysics(),
-        itemBuilder: (BuildContext context, int index) {
-          return MoviesItemWidget(_paginatedMovies.movies[index]);
-        });*/
-  }
-}
-
-class MyCollapsingImage extends SliverPersistentHeaderDelegate {
-  final String _image;
-
-  MyCollapsingImage(this._image);
-
-  @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Image.network(
-      _image,
-      fit: BoxFit.cover,
-    );
-  }
-
-  @override
-  double get maxExtent => 300.0;
-
-  @override
-  double get minExtent => 0.0;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
   }
 }
